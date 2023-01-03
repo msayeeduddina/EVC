7f99a506e964b67a7ba08bcd5ab478b256d85573
SAYEED <90544352+msayeeduddina@users.noreply.github.com>
2023-01-03 12:54:43 +0530

    Create MyNFT.sol

---
+++ EVC_old/MyNFT.sol
@@ -0,0 +1,19 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.0;
+
+import "@openzeppelin/contracts@4.6.0/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts@4.6.0/access/Ownable.sol";
+
+contract MyNFT is ERC721, Ownable {
+
+    uint256 public totalSupply;
+
+    constructor() ERC721("MyNFT", "MNFT") {}
+
+    function safeMint(address to) public {
+        totalSupply++;
+        _safeMint(to, totalSupply);
+    }
+
+}
